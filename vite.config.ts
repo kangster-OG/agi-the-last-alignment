57eb6682123c0a95e4ca62ce92f6df422efe0171
Dongho Kang <DKang2000@users.noreply.github.com>
2026-05-01 08:53:32 -0400

    Inline production dynamic imports for Render

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true
+      }
+    }
+  },
   server: {
     host: "127.0.0.1",
     port: 5173
